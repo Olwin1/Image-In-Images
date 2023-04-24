b71048ad9de6618496d46fb5f49aa736b437b5cc
Olwin1 <oliver_munn@icloud.com>
2023-04-24 18:45:30 +0100

    Added comments

---
+++ README.md
@@ -0,0 +1,4 @@
+##Image in images
+A simple program to replace every pixel of an image with another whole image making an image made of smaller images.
+To use just replace the "larger_image.png" file with a png of the image that you want to have smaller images be made of and the "smaller_image.png" with the small images you want to use.
+Do not use large images I recommend max of around 300px for larger image and 100px for smaller image as this takes a while to generate and every single pixel in the larger image will be replaced with the number of pixels in the smaller image.  
--- main.py
+++ main.py
@@ -1,43 +1,66 @@
+# Import the necessary libraries
 from PIL import Image
 import numpy as np
-picture = Image.open("./images/***REMOVED***")
-original = Image.open("./images/***REMOVED***")
 
-# Get the size of the image
+# Open the input images
+picture = Image.open("./images/smaller_image.png")
+original = Image.open("./images/larger_image.png")
+
+# Get the size of the input images
 width, height = picture.size
 o_width, o_height = original.size
 
-# Process every pixel
+# Define a function to calculate the color of each pixel
 def calcColour(a, b):
 
+    # Define a function to average two values
     def avg(w, z):
         numb = w + z + z
         return round(numb / 3)
-    return [avg(a[0], b[0]), avg(a[1], b[1]), avg(a[2], b[2])]
-picture2 = Image.open("./images/***REMOVED***")
+    
+    # Calculate the average of the RGB values of the two input pixels
+    return [avg(a[0], b[0]), avg(a[1], b[1]), avg(a[2], b[2]), 255]
 
+# Define a function to create a new pixel based on a given color
 def createPixel(newcolour):
-    array = np.array(picture2)
+    
+    # Convert the input image to a numpy array for faster processing
+    array = np.array(picture)
+    
+    # Iterate over every pixel in the image and apply the new color
     for x in range(len(array[0][0])):
         for y in range(len(array[0])):
             current_color = array[x][y]
-            ####################################################################
-            # Change Pixel Colour
-            ####################################################################
             array[x][y] = calcColour(current_color, newcolour)
+    
+    # Convert the numpy array back to an image and return it
     return Image.fromarray(np.uint8(array))
-    #picture.show()
+
+# Create a new blank image with the dimensions of the combined images
 final = Image.new('RGB', (picture.width * original.width, picture.height * original.height))
+
+# Define a starting point for pasting the new pixels
 start = [0, 0]
 
+# Iterate over every pixel in the original image
 for x in range(int(o_width)):
     print(str(round(int(x) / original.width * 100)) + "% Complete")
     for y in range(int(o_height)):
-        final.paste(createPixel(original.getpixel((x, y))), (start[0], start[1]))
-        #final.paste(createPixel((255, 0, 128)), (start[0], start[1]))
+        
+        # Calculate the new pixel based on the color of the current pixel in the original image
+        new_pixel = createPixel(original.getpixel((x, y)))
+        
+        # Paste the new pixel onto the final image at the current position
+        final.paste(new_pixel, (start[0], start[1]))
+        
+        # Move the position down by the height of the input image
         start[1] += height
+    
+    # Reset the position to the top and move it over by the width of the input image
     start[1] = 0
     start[0] += width
+
+# Save the final image to disk
 print("Saving...")
-final.save("./output2.png")
-print("Successfully Saved As output2.png")
+final.save("./output.png")
+print("Successfully Saved As output.png")
